46a00e1b3c77e50b015b688c4e9382d49dce05b3
MLHocalhost <18cy039@esisalama.org>
2020-04-11 18:11:40 +0200

    en setting

--- app.js
+++ app.js
@@ -15,20 +15,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
+app.get('*', function(req, res){
+    res.render('404', )
+})
+
 app.disable('etag');
 
-app.use(function(req, res, next) {
-    next(createError(404));
-    res.render('404');
+http.listen(3000, ()=>{
+    console.log('Example listening on port 3000!');
 });
-
-app.use(function(err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    res.status(err.status || 500);
-    res.render('500');
-});
-
-module.exports = app;
-module.exports = io;
